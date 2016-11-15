e6f767aeb18d4f7fa8908e9221072aa240c2c24a
Luca Faggianelli <luca.faggianelli@gmail.com>
2016-11-15 17:01:23 +0100

    Handle empty description

--- gitissues.py
+++ gitissues.py
@@ -21,7 +21,7 @@ class GitIssues(object):
             print(Fore.YELLOW + 'issue #{0[id]} {0[title]}'.format(issue) + Style.RESET_ALL)
             print('Author: {0[author]} <{0[author_email]}>'.format(issue))
             print('Date: {}'.format(issue['created_at'].strftime('%a, %d %b %Y %H:%M:%S %Z')))
-            if 'description' in issue:
-                print('')
+            print('')
+            if 'description' in issue and issue['description'].strip():
                 print(textwrap.fill(issue['description'], initial_indent=' '*4, subsequent_indent=' '*4))
                 print('')
