70d04150de67e6e60e72f45e8f0c4fff00fb1655
Luca Faggianelli <luca.faggianelli@gmail.com>
2016-11-15 12:39:53 +0100

    List gitlab issues

--- gitlabissues.py
+++ gitlabissues.py
@@ -1,5 +1,8 @@
 import gitlab
+import textwrap
 
+from colorama import Fore, Style
+from datetime import datetime
 from gitissues import GitIssues
 
 
@@ -9,9 +12,16 @@ class GitLabIssues(GitIssues):
 
         private_token = self.repo_config.get_value('gitlab', 'token')
         self.gl = gitlab.Gitlab(self.git_server, private_token, ssl_verify=False)
+        #self.gl.enable_debug()
         self.gl.auth()
 
     def list_issues(self):
         project = self.gl.projects.get(self.git_project)
         for issue in project.issues.list(state='opened'):
-            print(issue.title)
+            print(Fore.YELLOW + 'issue #{} {}'.format(issue.id, issue.title) + Style.RESET_ALL)
+            print('Author: {} <{}>'.format(issue.author.name, self.gl.users.get(issue.author.id).email))
+            print('Date: {}'.format(datetime.strptime(issue.created_at, '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%a, %d %b %Y %H:%M:%S %Z')))
+            print('')
+            if issue.description:
+                print(textwrap.fill(issue.description, initial_indent=' '*4, subsequent_indent=' '*4))
+                print('')
